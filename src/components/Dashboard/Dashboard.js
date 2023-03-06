f3ce81609800acf0119ccf59a13815d5469adc9c
nataliamachella <nataliamachella@gmail.com>
2023-03-06 09:59:40 -0300

    agregué el saludo al Dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -7,35 +7,54 @@ import iconApple from "../../assets/apple.png";
 import iconEdit from "../../assets/edit.png";
 import iconHospital from "../../assets/hospital.png";
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/authContext";
 
 const Dashboard = () => {
   const navigate = useNavigate();
+  const { user } = useAuth();
+
+  const onClickAboutMe = () => {
+    navigate("/nosotros");
+  };
+  const onClickRecipes = () => {
+    navigate("/recetas");
+  };
+  const onClickGroup = () => {
+    navigate("/comunidad");
+  };
+  const onClickDoctors = () => {
+    navigate("/medicos");
+  };
+
   return (
     <div className="dashboard">
+      <h1>Hola, {user.displayname || user.email}!</h1>
+      <a href="/">Cerrar Sesión</a>
       <img src={logo} className="logo" />
+
       <Button
         text="nosotros"
         img={iconEdit}
         style="secondary-btn"
-        onClick={() => navigate("/nosotros")}
+        onClick={onClickAboutMe}
       />
       <Button
         text="recetas"
         img={iconApple}
         style="green-btn"
-        onClick={() => navigate("/recetas")}
+        onClick={onClickRecipes}
       />
       <Button
         text="comunidad"
         img={iconComunidad}
         style="primary-btn"
-        onClick={() => navigate("/comunidad")}
+        onClick={onClickGroup}
       />
       <Button
         text="médicos"
         img={iconHospital}
         style="blue-btn"
-        onClick={() => navigate("/medicos")}
+        onClick={onClickDoctors}
       />
     </div>
   );
